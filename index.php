6ea788d6363268041f89eeff48315086e58ce2d5
Fildadubcek <dubcekf@gmail.com>
2021-04-06 11:24:27 +0200

    index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <?php
 $vodkaprice = "119 Kč";
 $homelessMoney = "25" + "17" + "40" + "100" - "50 ";
+$homelessMoneyafter = $homelessMoney - "119";
 echo "Cena vodky:". $vodkaprice;
 
 echo " <br>Peníze bezdomovce před návštěvou večerky:". $homelessMoney . "<br>";
-if ($homelessMoney > $vodkaprice) {echo "Bezdomovec si může koupit vodku";}
-elseif ($homelessMoney < $vodkaprice) {echo "Bezdomovec si nemůže koupit vodku";}
-
+if ($homelessMoney > $vodkaprice) {echo "Vodka zakoupena!";}
+if ($vodkaprice > $homelessMoney) {echo "Nedostatek peněz!";}
+echo "<br>Peníze po návštěvě večerky:". $homelessMoneyafter . "<br>";
 ?>
